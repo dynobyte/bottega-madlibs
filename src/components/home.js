47ee90d951299f7a627ac576fd72ae333aa3a5e0
dino soifua <dinopearson801@gmail.com>
2018-06-15 15:56:03 -0600

    Finished Madlibs Game

--- src/components/home.js
+++ src/components/home.js
@@ -3,15 +3,21 @@ import React, { Component } from 'react';
 import Header from './header';
 import Card from './card';
 
- class Home extends Component {
+class Home extends Component {
   render() {
     return (
       <div className="home">
-       {Header()}
-       <Card/>
+        { Header() }
+        <Card />
       </div>
     );
   }
 }
 
+// things we need to fix
+
+// content labels
+// generate btn space/card-height
+
 export default Home;
+
