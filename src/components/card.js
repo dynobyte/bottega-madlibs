5d8f4643058f76754e4e16cb37edafd65646bb98
dino soifua <dinopearson801@gmail.com>
2018-06-15 08:34:39 -0600

    added numbers to content

--- src/components/card.js
+++ src/components/card.js
@@ -78,7 +78,9 @@ class Card extends Component {
             <form onSubmit={this.handleFormSubmit} className="card">
                 <div className="card__input">
                 {
-                   inputData.map(data => Input( (data), this.handleInputChange ))
+                   inputData.map((data, index) => { 
+                       return Input( (data), this.handleInputChange, index ) 
+                    })
                 }
                 </div>
                 <button type="submit">{!this.state.contentVisible ? `Generate Madlib` : `Clear Form`}</button>
